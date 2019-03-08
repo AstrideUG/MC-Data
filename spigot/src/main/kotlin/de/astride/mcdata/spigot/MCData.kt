696d250cc51449d7cc339d48f632764098b50f26
LartyHD <lartyhd@gmail.com>
2019-03-08 19:35:21 +0100

    Dependency configuration

---
+++ spigot/src/main/kotlin/de/astride/mcdata/spigot/MCData.kt
@@ -0,0 +1,13 @@
+package de.astride.mcdata.spigot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 08.03.2019 11:58.
+ * Current Version: 1.0 (08.03.2019 - 08.03.2019)
+ */
+class MCData : JavaPlugin() {
+
+
+}
